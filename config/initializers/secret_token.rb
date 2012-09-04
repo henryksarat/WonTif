65286e24e45190c462a92b974e8c717f8fbad0de
henryk <henryk@aol.com>
2012-09-04 17:23:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WonTif::Application.config.secret_token = 'c881d1f2cb8c9e65676de3d27f954bb6ec9a1f54e4770ecc539078311c01e8a6645eec5e9ac1ec989f427325a706e5ac2347b3f7554bf280d140568d4eb7228b'
