1303a7acf4994dc26e14e45f7c52b10369511617
henryk <henryk@aol.com>
2012-09-05 16:36:26 -0500

    added example migrations files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905185121) do
+ActiveRecord::Schema.define(:version => 20120905193530) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(:version => 20120905185121) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "products", :force => true do |t|
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "description"
+    t.string   "name"
   end
 
   create_table "tags", :force => true do |t|
