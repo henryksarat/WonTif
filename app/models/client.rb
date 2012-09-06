522ce4aa81734d8f4d2f2f896622d9bca130b866
henryk <henryk@aol.com>
2012-09-06 13:34:28 -0500

    removed files

--- app/models/client.rb
+++
@@ -1,5 +0,0 @@
-class Client < ActiveRecord::Base
-  has_one :addresses
-  has_many :orders
-  has_and_belongs_to_many :roles
-end
--- app/models/order.rb
+++
@@ -1,3 +0,0 @@
-class Order < ActiveRecord::Base
-  belongs_to :client, :counter_cache => true
-end
