89f5a6f1aa9489d8190ecab429b2c970824117e7
henryk <henryk@aol.com>
2012-09-06 16:58:31 -0500

    added uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,17 @@ class User < ActiveRecord::Base
   
   validates :email, :presence => true,
   		    :confirmation => true,
+  		    :uniqueness => true,
   		    :format => { 
   		    	:with => /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/, 
   		    	:message => " invalid!!"
   		    }    
-  validates :first_name, :presence => true
-  validates :last_name, :presence => true
+  validates :first_name, :presence => true, :length => { :in => 1..10 }
+  validates :last_name, :presence => true,
+  			:exclusion => {
+  				:in => %w(obama),
+  				:message => "Cuz I know you ain't %{value}!" 
+  			}
   validates :terms_of_service, :acceptance => true
   validates :email_confirmation, :presence => true
 end
