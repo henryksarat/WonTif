a5b7be8ccb068be53407886655726097549f9c01
henryk <henryk@aol.com>
2012-09-07 11:17:39 -0500

    added custom validator

---
+++ app/helpers/GoodnessValidator.rb
@@ -0,0 +1,7 @@
+class GoodnessValidator < ActiveModel::Validator
+  def validate(record)
+    if record.first_name == "Evil"
+      record.errors[:base] << "This person is evil"
+    end
+  end
+end
