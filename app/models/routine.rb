bf5001762483ef3834c4740ef4adf7d85bdb9c18
henryksarat <henryk@aol.com>
2012-09-10 10:20:51 -0500

    added exmaple of callback on association

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,7 +1,15 @@
 class Routine < ActiveRecord::Base
+  attr_accessible :description, :name
+  
   belongs_to :users
   has_and_belongs_to_many :workouts
-  has_many :RoutinesWorkouts
+  has_many :RoutinesWorkouts, :after_add => [:new_routine, :email_routine]
   
-  attr_accessible :description, :name
+  def new_routine(rworkout)
+   puts "added a new routine workout"  
+  end
+
+  def email_routine(rworkout)
+   puts "now email the routine"  
+  end
 end
