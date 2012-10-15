37ae89efe560ab9e447b6d7bd7ac83451d207c3d
henryksarat <henryk@aol.com>
2012-10-15 10:21:05 -0500

    reading URL

--- custom-script.rb
+++ custom-script.rb
@@ -1,1 +1,6 @@
+require 'open-uri'
+
 puts "hi there"
+
+contents = URI.parse('http://ec2-204-236-130-153.us-west-1.compute.amazonaws.com/policy/0fcc9330-cb04-012f-949a-1231400142dd/52931786e6ad34f1bbfb126d5ed93ec2a7678847/com.test.api/policy.xml').read
+puts contents
